0fcfe82a7188f3660a1fa4efc1a23f561fda53a9
Sergei <isaenko.sv@gmail.com>
2023-03-23 22:30:11 +0300

    added struct Stop {} and more

--- sprint10transport/transport_catalogue.cpp
+++ sprint10transport/transport_catalogue.cpp
@@ -33,7 +33,8 @@ const std::string& transport_catalogue::FindBus(const std::string& name) const {
 const Coordinates& transport_catalogue::FindStop(const std::string& name) const {
 	if (stops_.count(name) == 0)
 		throw std::logic_error("Search of non-existent stop");
-	return stops_.at(name);
+	const Stop* tmp = &*stops_.find(name);
+	return tmp->coords_;
 }
 
 void transport_catalogue::ParseRoutes() {
@@ -64,21 +65,51 @@ void transport_catalogue::ParseRoutes() {
 transport_catalogue::Route transport_catalogue::ParseLinearRoute(
 	const std::string& raw_desc) {
 
+	size_t j = raw_desc.size();
 	Route ret;
-	std::vector<size_t> delim_pos;
+	ret.way_.reserve(j / 5);
+	size_t bgn = 0, end = 0, sz = 0;
 
-	// need to reforge it
-	for (size_t i = 0, j = raw_desc.size(); i < j; ++i) {
-		if (raw_desc[i] == BusLineDelimeter) {
+	for (size_t i = 0; i < j; ++i) {
+		if (raw_desc[i] == BusLineDelimeter || i == j - 1) {
 			++ret.unique_stops_;
-			delim_pos.push_back(i);
+			end = (i == j - 1) ? j + 1 : i;
+			sz = end - bgn - 1;
+			ret.way_.push_back({&raw_desc[bgn], sz});
+			bgn = end + 2;
 		}
 	}
 
-	return Route();
+	// adding tail
+	ret.way_.reserve(ret.way_.size() * 2);
+	ret.way_.insert(ret.way_.end(), ret.way_.rbegin() + 1, ret.way_.rend());
+
+	ret.way_.shrink_to_fit();
+	//ret.length = ret.way_.size();	// some method
+	return ret;
 }
 
 transport_catalogue::Route transport_catalogue::ParseCircularRoute(
 	const std::string& raw_desc) {
-	return Route();
+
+	size_t j = raw_desc.size();
+	Route ret;
+	ret.way_.reserve(j / 5);
+	size_t bgn = 0, end = 0, sz = 0;
+
+
+
+	return ret;
+}
+
+void transport_catalogue::CalcDistances(Route& route) {
+
+}
+
+bool transport_catalogue::Stop::operator<(const Stop& other) const {
+	return name_ < other.name_;
+}
+
+bool transport_catalogue::Stop::operator==(const Stop& other) const {
+	return name_ == other.name_;
 }
--- sprint10transport/transport_catalogue.h
+++ sprint10transport/transport_catalogue.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <string>
 #include <string_view>
+#include <functional>
 
 #include "geo.h"
 
@@ -11,17 +13,48 @@ class transport_catalogue {
 private:			// structs
 	struct Route {
 		std::vector<std::string_view> way_;
-		size_t unique_stops_ = 1;
+		size_t unique_stops_ = 0;
 		double length = 0;
+	};
+
+	struct Stop {
+		std::string name_;
+		Coordinates coords_;
+
+		// constructor for searching by the name with calling stops_.count(name)
+		Stop(const std::string& name = "", const Coordinates& coords = Coordinates{})
+			: name_(name), coords_(coords) { };
+
+		//Stop(const Stop&) = default;
+		//Stop(Stop&&) = default;
+		//Stop& operator=(const Stop&) = default;
+		//~Stop() = default;
+
+		bool operator<(const Stop& other) const;
+		bool operator==(const Stop& other) const;
+	};
+
+	struct StopHasher {
+		size_t operator()(const Stop& stop) const {
+			return static_cast<size_t>(
+				std::hash<std::string>{}(stop.name_)
+				//static_cast<size_t>(stop.coords_.lat) * 97 +
+				//static_cast<size_t>(stop.coords_.lng) * 97 * 97
+				);
+		}
 	};
 
 private:			// fields
 	// HINT : u_map < stop name , coords >
-	std::unordered_map<std::string, Coordinates> stops_;
+	//std::unordered_map<std::string, Coordinates> stops_;
+	// HINT : u_set < struct { string name , Coords } >
+	std::unordered_set<Stop, StopHasher> stops_;
 	// HINT : u_map < bus name , raw route >
 	std::unordered_map<std::string, std::string> buses_;
 	// HINT : u_map < bus name , vector < string_view Stops> >
 	std::unordered_map<std::string, Route> routes_;
+
+
 
 	const char BusLineDelimeter = '-';
 	const char BusCircleDelimeter = '>';
@@ -44,5 +77,6 @@ private:			// methods
 	Route ParseLinearRoute(const std::string& raw_desc);
 	Route ParseCircularRoute(const std::string& raw_desc);
 
+	void CalcDistances(Route& route);
 };
 
